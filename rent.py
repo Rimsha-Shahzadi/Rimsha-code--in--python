46bb4c908d75bc520a2412e9e832e2f75cafcd86
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-09-18 15:58:33 +0500

    .

--- rent.py
+++ rent.py
@@ -2,15 +2,15 @@
 # First project
 
 
-# rent = int(input("Enter your hostel/flat rent: "))
-# food = int(input("Enter amount of  food ordered: "))
-# electricity_spend = int(input("Enter your electricity bill: "))
-# charge_per_unit = int(input("Enter charge per unit"))
-# persons = int(input("Enter the number of persons living in room/flat"))
+rent = int(input("Enter your hostel/flat rent: "))
+food = int(input("Enter amount of  food ordered: "))
+electricity_spend = int(input("Enter your electricity bill: "))
+charge_per_unit = int(input("Enter charge per unit"))
+persons = int(input("Enter the number of persons living in room/flat"))
 
-# total_bill = electricity_spend * charge_per_unit
-# output = (food + rent+ total_bill) // persons
-# print("Each person will pay ", output)
+total_bill = electricity_spend * charge_per_unit
+output = (food + rent+ total_bill) // persons
+print("Each person will pay ", output)
 
 # Srcond project
 
