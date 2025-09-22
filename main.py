b167a21808aa9ef157765d11c690a772d0ae2f1e
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-09-22 06:00:51 +0500

    .

--- main.py
+++ main.py
@@ -542,35 +542,51 @@
 
 # common()
 
-def task():
-    tasks = []
-    print("----Welcome to the Task Management App----")
+# def task():
+#     tasks = []
+#     print("----Welcome to the Task Management App----")
 
-    total_task = int(input("Enter how many task yo want to add: "))
-    for i in range(1,total_task+1):
-        task_name = input(f"Enter task {i} = ")
-        tasks.append(task_name)
+#     total_task = int(input("Enter how many task yo want to add: "))
+#     for i in range(1,total_task+1):
+#         task_name = input(f"Enter task {i} = ")
+#         tasks.append(task_name)
 
-    print(f"Today's tasks are\n{tasks}")  
+#     print(f"Today's tasks are\n{tasks}")  
 
-    while True:
-       operation = int(input("Enter 1-Add\n2-Update\n3-Delete\n4-View\n5-Exit/Stop/"))
-       if operation == 1:
-          add = input("Enter task you want to add:")
-          tasks.append(add)
-          print(f"Task {add} has been added successfully....")
+#     while True:
+#        operation = int(input("Enter 1-Add\n2-Update\n3-Delete\n4-View\n5-Exit/Stop/"))
+#        if operation == 1:
+#           add = input("Enter task you want to add:")
+#           tasks.append(add)
+#           print(f"Task {add} has been added successfully....")
 
-       elif operation == 2:
-           updated_val = input("Enter task name you want to update:")
-           if updated_val in tasks:
-               up = input("Enter new task:")
-               ind = tasks.index(updated_val)
-               tasks[ind] = up
-               print(f"updated task {up}")
+#        elif operation == 2:
+#            updated_val = input("Enter task name you want to update:")
+#            if updated_val in tasks:
+#                up = input("Enter new task:")
+#                ind = tasks.index(updated_val)
+#                tasks[ind] = up
+#                print(f"updated task {up}")
 
     
 
+# start
 
+dictionary = {
+    "name": "Rimsha",
+    "age": 24,
+    "city": "Shahkot",
+    "course": "Python",
+    "password": "Rimsha123",
+    "is_logged_in": True,
+    "result" : {
+        "english": 90,
+        "maths": 95,
+        "science": 85
+    }
+}
+dictionary.clear()
+print(dictionary)
 
 
 
