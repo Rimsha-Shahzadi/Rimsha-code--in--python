4e2830b582f361a6e409bef11abd08c9a7e8a200
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-10-07 08:34:37 +0500

    calculator

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 # print("Hello Rimsha")
 # print(8+8)
 # # "Hello"[1:4]
-import time
-timestamp =time.strftime('%H:%M:%S')
-print(timestamp)
-timestamp =time.strftime('%H')
-print(timestamp)
+# import time
+# timestamp =time.strftime('%H:%M:%S')
+# print(timestamp)
+# timestamp =time.strftime('%H')
+# print(timestamp)
 
-timestamp =time.strftime('%M')
-print(timestamp)
+# timestamp =time.strftime('%M')
+# print(timestamp)
 
-timestamp =time.strftime('%S')
-print(timestamp)
-import time
-timestamp =time.strftime('Good Morning:Good Afternoon:Good Night:')
-print(timestamp)
-timestamp =time.strftime('Good Morning Sir')
-print(timestamp)
-timestamp =time.strftime('Good Afternoon Sir')
-print(timestamp)
-timestamp =time.strftime('Good Night Sir')
-print(timestamp)
+# timestamp =time.strftime('%S')
+# print(timestamp)
+# import time
+# timestamp =time.strftime('Good Morning:Good Afternoon:Good Night:')
+# print(timestamp)
+# timestamp =time.strftime('Good Morning Sir')
+# print(timestamp)
+# timestamp =time.strftime('Good Afternoon Sir')
+# print(timestamp)
+# timestamp =time.strftime('Good Night Sir')
+# print(timestamp)
 
 
 # age =int(input("Enter your age: "))
