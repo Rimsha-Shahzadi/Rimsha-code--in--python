b319a2a88aa12bf272e307b0d42c896991cf0c16
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-10-28 18:23:45 +0500

    Done

---
+++ quiz.py
@@ -0,0 +1,21 @@
+score = 0
+print("Q1 : What is the capital of France? : ")
+ans1 = input()
+if ans1 == "Paris":
+    score += 1
+else:
+    print("Incorrect")
+
+print("Q1 : Is Antartica a cold place or hot? :  ")
+ans1 = input()
+if ans1 == "Cold":
+    score += 1
+else:
+    print("Incorrect")
+
+print("Q1 : Who is the king of jungle? : ")
+ans1 = input()
+if ans1 == "lion":
+    score += 1
+else:
+    print("Incorrect")
