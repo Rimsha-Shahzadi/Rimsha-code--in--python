95905ebd54d9103697531c954cde8b2e940d37d3
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-11-10 17:23:06 +0500

    .

--- star.py
+++ star.py
@@ -50,12 +50,50 @@
 # for i in range(1,11):
 #     print(f"{num} * {i} = {num*i}")
   
-def lines():
-    line = int(input("Enter a number of lines:"))
-    for i in range(1,line+1):
-        for j in range(i):
-            print("*", end = " ")
-        print()
-    return f"The {line} lines of triangle has beeen created"
-triangle = lines()
-print(triangle)
+# def lines():
+#     line = int(input("Enter a number of lines:"))
+#     for i in range(1,line+1):
+#         for j in range(i):
+#             print("*", end = " ")
+#         print()
+#     return f"The {line} lines of triangle has beeen created"
+# triangle = lines()
+# print(triangle)
+
+# num = int(input("Enter a number to print a table: "))
+
+# for i in range(1,11):
+#     print(f"{num}*{i} = {num*i}")
+
+#1
+n = 10
+for i in range(1, n+1):
+    print("*" * i)
+#2
+n = 10
+for i in range(1, n+1):
+    print("  " * (n-i) + " *" * i)    
+#3
+n = 10
+for i in range(1, n+1):
+    print(" " * (n-i) + "* " * i) 
+#4
+n = 10
+for i in range(n, 0, -1):
+    print(" " * (n-i) + "* " * i)
+#5
+n = 10
+for i in range(1, n+1):
+    print(" " * (n-i) + "* " * i)
+for i in range(n-1, 0, -1):
+    print(" " * (n-i) + "* " * i)
+#6
+n = 10
+for i in range(1, n+1):
+    print("* " *n) 
+#7
+n = 10
+for i in range(1, n+1):
+    for j in range(1,i+1):
+        print(j, end=" ") 
+    print()          
