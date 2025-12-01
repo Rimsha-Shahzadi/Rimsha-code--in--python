b2c731f10bc95b2386d03e64f922455e149be1f6
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-12-01 08:00:03 +0500

    .

---
+++ python/first.py
@@ -0,0 +1,12 @@
+# print("Hello world I am Rimsha and i am a student of PIAIC")
+# table = int(input("Enter a number to print a table: "))
+# for i in range(1,11):
+#     print(f"{table} * 1 = {table*i}")
+# rows = 5
+# for i in range(1,rows+1):
+    # print("* " * i)
+n = 10
+for i in range(1, n+1):
+    print(" " * (n-i) + "* " * i)
+for i in range(n-1, 0, -1):
+    print(" " * (n-i) + "* " * i)
