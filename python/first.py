5740126b5a942cca06d8191a5cdbf7cbfc0d0ac9
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-12-02 19:36:51 +0500

    first class

--- python/first.py
+++ python/first.py
@@ -1,9 +1,9 @@
-print("Hello world I am Rimsha and i am a student of PIAIC")
-print("Pakistan")
-print("Zinda Baad")
+# print("Hello world I am Rimsha and i am a student of PIAIC")
+# print("Pakistan")
+# print("Zinda Baad")
 # table = int(input("Enter a number to print a table: "))
 # for i in range(1,11):
-#     print(f"{table} * 1 = {table*i}")
+    # print(f"{table} * 1 = {table*i}")
 # rows = 5
 # for i in range(1,rows+1):
     # print("* " * i)
@@ -12,3 +12,38 @@ print("Zinda Baad")
     # print(" " * (n-i) + "* " * i)
 # for i in range(n-1, 0, -1):
     # print(" " * (n-i) + "* " * i)
+
+# print("Pakistan")
+# print("Zinda Baad")
+# print("Zinda Baad")
+# print("Zinda Baad")
+
+name: str = "Rimsha Shahzadi"
+print(name)
+print(type(name))
+print(id(name))
+
+name: int = 200
+print(name)
+print(type(name))
+print(id(name))  
+
+name: float = 20.5
+print(name)
+print(type(name))
+print(id(name))
+
+name: bool = True
+print(name)
+print(type(name))
+print(id(name))
+
+name: list[str] = ["Rimsha", "Jannat", "Ismail"]
+print(name)
+print(type(name))
+print(id(name))
+
+name: tuple[str, int, float] = ("Rimsha", 25, 25.5)
+print(name)
+print(type(name))
+print(id(name))
