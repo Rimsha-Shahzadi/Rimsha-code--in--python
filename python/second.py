7a6722a5257ddcdf603fd87b162295ddd4d50c42
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-12-03 17:39:53 +0500

    .

---
+++ python/second.py
@@ -0,0 +1,8 @@
+# table = int(input("Enter a number to print a table: "))
+# for i in range(1,11):
+#     print(f"{table}*{1} = {table*i}")
+
+# print("Rimsha","Jannat", "Ismail")
+# n = 10
+for i in range(11):
+  print(i)
