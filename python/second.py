ea3e0fe479b791a4d54869aa99cfa26d2cf2d693
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-12-09 10:18:25 +0500

    .

--- python/second.py
+++ python/second.py
@@ -4,5 +4,15 @@
 
 # print("Rimsha","Jannat", "Ismail")
 # n = 10
-for i in range(11):
-  print(i)
+# for i in range(11):
+#   print(i)
+
+# message: str = "PIAIC student card\n Father's name"
+# print(message)
+name: str = "Rimsha Shahzadi"
+fname: str = "Muhammad Arif"
+educaton: str = "Graduation"
+age : int = 24
+
+card : str = "PIAIC Student Card\nStudent Name:" + name
+print(card)
