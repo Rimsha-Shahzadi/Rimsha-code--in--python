25ad5539a50f2e11bbd009dfa5c92fda49a60f19
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-12-10 20:02:35 +0500

    .

--- python/second.py
+++ python/second.py
@@ -58,18 +58,18 @@
 # print(name)
 
 # [i for i in dir(str) if "__" not in i]
-name: str = "Rimsha Shahzadi"
-fname: str = "Muhammad Arif"
-education: str = "Graduation"
-age : int = 24
-card : str = """
-PIAIC Student Card
-Student Name: {0}
-Father's Name:  {1}
-Age: {3}
-Education: {2}
-""".format(name, fname, education, age)
-print(card)
+# name: str = "Rimsha Shahzadi"
+# fname: str = "Muhammad Arif"
+# education: str = "Graduation"
+# age : int = 24
+# card : str = """
+# PIAIC Student Card
+# Student Name: {0}
+# Father's Name:  {1}
+# Age: {3}
+# Education: {2}
+# """.format(name, fname, education, age)
+# print(card)
 # name : str = "Rimsha Shahzadi"
 # print(name.capitalize())
 # print(name.lower())
@@ -77,3 +77,35 @@ print(card)
 # a = 5
 # b = 10
 # print("pakistan valuea ={} and value b ={}".format(a,b))
+# Student_code: str = """
+# print("My Name is Rimsha")
+# a : int = 12
+# b : int = 12
+# print(a+b)
+# """
+# exec(Student_code)
+
+#Pre define global functions we have used
+#print
+#type
+#id
+#dir
+#len
+
+# a : list[str] = [i for i in dir(str) if "__" not in i]
+# print(a)
+# print(len(a))
+# name : str = "     Rimsha Shahzadi     "
+# print(name)
+# print(name.lstrip())
+# print(name.rstrip())
+
+# import re
+# name : str = "   Rimsha     Shahzadi    "
+# print(name)
+# name1 : str = re.sub(' {2,100}', ' ', name).strip()
+# print(name1)
+
+name : str = "   Rimsha     Shahzadi    "
+print(name)
+print(name.title())
