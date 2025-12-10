58c3eb0b59e12eea8ca82dfc6bf5e7371df753eb
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-12-10 08:48:16 +0500

    .

--- python/second.py
+++ python/second.py
@@ -9,10 +9,31 @@
 
 # message: str = "PIAIC student card\n Father's name"
 # print(message)
-name: str = "Rimsha Shahzadi"
-fname: str = "Muhammad Arif"
-educaton: str = "Graduation"
-age : int = 24
+# name: str = "Rimsha Shahzadi"
+# fname: str = "Muhammad Arif"
+# educaton: str = "Graduation"
+# age : int = 24
 
-card : str = "PIAIC Student Card\nStudent Name:" + name
-print(card)
+# card : str = "PIAIC Student Card\nStudent Name:" + name +\
+# "\nAge:" + str(age) +"\n"+\
+# "Education: " + educaton
+
+# print(card)
+
+# print(1+8+9)
+# print(1+ \
+#       8+ \
+#         3)
+
+# Multiline string
+# card : str = """
+# PIAIC Student Card
+# Student Name: Rimsha Shahzadi
+# Age: 24
+# Education: Graduation
+
+# """
+# print(card)
+
+#F-STRING Python
+
