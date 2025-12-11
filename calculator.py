439375c71b6251fff0f8a73fb19c17a91059186b
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-12-11 07:40:48 +0500

    .

--- calculator.py
+++ calculator.py
@@ -9,7 +9,7 @@ def divide(num1, num2):
 def average(num1, num2):
     return num1 + num2 /2
 
-print("Please select a operation: \n " \
+print("Please select an operation: \n " \
       "1. Addition\n " \
       "2. Subtraction\n " \
       "3. Multiply\n " \
@@ -36,3 +36,24 @@ elif select ==5:
         average(number1,number2))
 else:
     print("Invalid operation please try again")    
+
+# Practice Calculator
+# def add(num1,num2):
+#     return num1 + num2 
+# def sub(num1,num2):
+#     return num1 - num2 
+# def divide(num1,num2):
+#     return num1 / num2 
+# def multiply(num1,num2):
+#     return num1 * num2 
+# def average(num1,num2):
+#     return num1 + num2 / 2 
+# print("Please selrct an operator: \n " \
+#       "1. Addition\n " \
+#       "2. Subtraction\n " \
+#       "3. Multiply\n " \
+#       "4. Divide\n " \
+#       "5. Average\n " ) 
+# select = int(input("Select an operation(1,2,3,4,5): "))
+# number1 = int(input("Enter first number: "))
+# number2 = int(input("Enter second number: "))
--- star.py
+++ star.py
@@ -97,3 +97,8 @@ for i in range(1, n+1):
     for j in range(1,i+1):
         print(j, end=" ") 
     print()          
+
+
+rows = 5
+for i in range(1, rows+1):
+    print(" *" * i)
