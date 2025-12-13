12bbdb6f529f07f20d65b4746ac0863b1ce1b375
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-12-13 07:45:19 +0500

    .

--- python/third.py
+++ python/third.py
@@ -1,3 +1,13 @@
 import pandas as pd
 table = pd.read_html("https://www.w3schools.com/python/python_operators.asp")
-table[0]
+print(table)
+print(table[0])
+  
+# import pandas as pd
+# df = pd.read_html("https://www.w3schools.com/python/python_operators.asp")
+# print(len(df), "tables found")
+# print(df[0])
+
+
+
+
