70e212499027b7bc6b4faecf2e25b0c1c4c0dd0f
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-12-31 17:18:56 +0500

    .

--- table.py
+++ table.py
@@ -1,4 +1,12 @@
-table = int(input("Enter a number to print  a table : "))
-for i in range(1,11):
-    print(f"{table} * {i} = {table *i}")
+# table = int(input("Enter a number to print  a table : "))
+# for i in range(1,11):
+#     print(f"{table} * {i} = {table *i}")
     
+
+# table = int(input("Enter a number to print a table :"))  
+# for i in range(1,11):
+#     print(f"{table} * {i} = {table*i}") 
+
+stars = 5
+for i in range(1, stars+1):
+    print(" *" * i)
