2fd052b89ab736312e8eb9412d5e7beeb36e7403
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-01-06 18:43:14 +0500

    .

--- new.py
+++ new.py
@@ -4,3 +4,8 @@ speed(0)
 
 # calculator = input("Enter number: ")
 # print(eval(calculator))
+star = 3
+for i in range(1,star+1):
+    print(" *" * i)
+for i in range(star-1,0,-1):
+    print(" *" * i)
