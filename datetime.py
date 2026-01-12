41d05fd0a49adcfd0654587a856206bae6597f8e
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-01-12 18:44:14 +0500

    .

---
+++ datetime.py
@@ -0,0 +1,34 @@
+# from turtle import *
+# from datetime import datetime
+# bgcolor("white")
+# hideturtle()
+# penup()
+# today =datetime.now().strftime("%Y-%m-%d")
+
+# goto(0,0)
+# write(f"Today's Date: {today}",
+#     align="center",
+#     font =("Arial",18,
+#     "bold"))
+# done()
+
+
+import turtle as t
+import random
+t.setup(900,900)
+t.bgcolor("black")
+t.speed(0)
+t.tracer(0)
+for i in range(300):
+   t.penup()
+   t.goto(0,0)
+   t.setheading(random.randint(0,360))
+   t.pendown()   
+import random
+   t.pencolor(random.random(), random.random(), random.random())
+   t.forward(i*2)
+   t.dot(3)
+   if i % 5 ==0:
+      t.update()
+      t.hideturtle()
+      t.done()
--- new.py
+++ new.py
@@ -1,6 +1,6 @@
-from turtle import *
-from colorsys import *
-speed(0)
+# from turtle import *
+# from colorsys import *
+# speed(0)
 
 # calculator = input("Enter number: ")
 # print(eval(calculator))
@@ -9,16 +9,31 @@ speed(0)
 #     print(" *" * i)
 # for i in range(star-1,0,-1):
 #     print(" *" * i)
-calculator = input("Enter a number: ")
-print(eval(calculator))
-star = 3
-for i in range(1, star+1):
-    print(" *" * i)
-for i in range(star-1,0 ,-1):
-    print(" *" * i)
-calender = input("Enter a year:")
-print(eval(calender))
+# calculator = input("Enter a number: ")
+# print(eval(calculator))
+# star = 3
+# for i in range(1, star+1):
+#     print(" *" * i)
+# for i in range(star-1,0 ,-1):
+#     print(" *" * i)
+# import calender 
+# print(calender.calender(2026))
 
-for i in range(1,11):
-    for j in range(1,11):
-        print(f'{i} * {j} = {i*j}',end='\t')
+# for i in range(1,11):
+#     for j in range(1,11):
+#         print(f'{i} * {j} = {i*j}',end='\t')
+
+
+from turtle import *
+from datetime import datetime
+bgcolor("white")
+hideturtle()
+penup()
+
+today = datetime.now().strftime("%Y-%m-%d")
+goto(0, 0)
+write(f"Today's Date: {today}",
+        align="center",
+        font=("Arial", 18, "bold"))
+
+done()
