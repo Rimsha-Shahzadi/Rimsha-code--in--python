65db4441cd3e60459cdf79c75ea309b1c9eea72e
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-01-21 12:17:35 +0500

    .

--- temp.py
+++ temp.py
@@ -8,12 +8,12 @@
 # else:
 #     print("Invsalid input try again: ")
 
-star = 3
-for i in range(1,star+1):
-    print("* " * i)
-calculator = input("Enter a number:")
-print(eval(calculator))
+# star = 3
+# for i in range(1,star+1):
+#     print("* " * i)
+# calculator = input("Enter a number:")
+# print(eval(calculator))
 
-table = input("Enter a number to print a table:  ")
+table = int(input("Enter a number to print a table:  "))
 for i in range(1,11):
     print(f"{table} * {i} = {table*i}")
