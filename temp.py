05e944e134a14d24479b7d8da51b0e7cac896e43
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-01-21 11:04:07 +0500

    .

--- temp.py
+++ temp.py
@@ -1,10 +1,12 @@
-temp = float(input("Enter the temperature: "))
-if temp <=36:
-    print("Cold temperature:  ")
-elif temp <=37.5:
-    print("Normal temperature: ") 
-elif temp > 37.5:
-    print("Hot temmperature: ")
-else:
-    print("Invsalid input try again: ")
+# temp = float(input("Enter the temperature: "))
+# if temp <=36:
+#     print("Cold temperature:  ")
+# elif temp <=37.5:
+#     print("Normal temperature: ") 
+# elif temp > 37.5:
+#     print("Hot temmperature: ")
+# else:
+#     print("Invsalid input try again: ")
+
+
 
