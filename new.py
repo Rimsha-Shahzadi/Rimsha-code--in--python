c253cb1913f761466aba962abb162d43f50b49fd
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-01-28 18:16:23 +0500

    .

--- new.py
+++ new.py
@@ -46,9 +46,42 @@
 # print(calendar.calendar(2026))
 # calculator = input("Enetr a number: ")
 # print(eval(calculator))
-name: str = "Rimsha Shahzadi"
-print(name)
-print(type(name))
-print(id(name))
-print(dir(name))
-print([i for i in dir(name) if "__" not in i])
+# name: str = "Rimsha Shahzadi"
+# print(name)
+# print(type(name))
+# print(id(name))
+# print(dir(name))
+# print([i for i in dir(name) if "__" not in i])
+
+# from tkinter import *
+# from PIL import Image, ImageTk
+# root = Tk()
+# root.geometry("500x500")
+# image  =Image.open('img.jpg')
+# photo = ImageTk.PhotoImage(image)
+# Label(image=photo).pack()
+# root.mainloop()
+
+from datetime import date
+def calculate_age(birth_year,birth_month,birth_day):
+    today = date.today()
+    birth_date = date(birth_year, birth_month, birth_day)
+    age_years = today.year - birth_date.year
+    age_month = today.month - birth_date.month
+    age_days = today.day - birth_date.day
+    if age_days < 0:
+        age_month -=1
+        age_days +=30
+    if age_month < 0:
+        age_years -=1
+        age_month +=12
+    return age_years, age_month, age_days
+print("____Age Calculator____")
+
+year = int(input("Enter your birth year (YYYY): "))
+month = int(input("Enter your birth month (MM): "))
+day = int(input("Enter your birth day (DD): "))
+year,month,day = calculate_age(year, month, day)
+print(f"\n Your age is: ")
+print(f"Your Age is {year} years, {month} months, and {day} days.")
+
