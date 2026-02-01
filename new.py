c7cc1b23041b5bab9ad05d56afe65b5a6ffffab3
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-02-01 11:16:24 +0500

    .

--- new.py
+++ new.py
@@ -62,26 +62,28 @@
 # Label(image=photo).pack()
 # root.mainloop()
 
-from datetime import date
-def calculate_age(birth_year,birth_month,birth_day):
-    today = date.today()
-    birth_date = date(birth_year, birth_month, birth_day)
-    age_years = today.year - birth_date.year
-    age_month = today.month - birth_date.month
-    age_days = today.day - birth_date.day
-    if age_days < 0:
-        age_month -=1
-        age_days +=30
-    if age_month < 0:
-        age_years -=1
-        age_month +=12
-    return age_years, age_month, age_days
-print("____Age Calculator____")
+# from datetime import date
+# def calculate_age(birth_year,birth_month,birth_day):
+#     today = date.today()
+#     birth_date = date(birth_year, birth_month, birth_day)
+#     age_years = today.year - birth_date.year
+#     age_month = today.month - birth_date.month
+#     age_days = today.day - birth_date.day
+#     if age_days < 0:
+#         age_month -=1
+#         age_days +=30
+#     if age_month < 0:
+#         age_years -=1
+#         age_month +=12
+#     return age_years, age_month, age_days
+# print("____Age Calculator____")
 
-year = int(input("Enter your birth year (YYYY): "))
-month = int(input("Enter your birth month (MM): "))
-day = int(input("Enter your birth day (DD): "))
-year,month,day = calculate_age(year, month, day)
-print(f"\n Your age is: ")
-print(f"Your Age is {year} years, {month} months, and {day} days.")
+# year = int(input("Enter your birth year (YYYY): "))
+# month = int(input("Enter your birth month (MM): "))
+# day = int(input("Enter your birth day (DD): "))
+# year,month,day = calculate_age(year, month, day)
+# print(f"\n Your age is: ")
+# print(f"Your Age is {year} years, {month} months, and {day} days.")
 
+calculator = input("Enter a number: ")
+print(eval(calculator))
