a8da41fc19dd7cbdd3fdb24a9ae1dc630a3a59d6
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-02-03 16:13:06 +0500

    .

--- song.py
+++ song.py
@@ -4,3 +4,10 @@ for i in Song:
     sys.stdout.write(i)
     sys.stdout.flush()
     time.sleep(0.1)
+
+import sys, time
+Song = "\nMeri Zindghi Hai Tu\n Tu Jo Chahy Bol Na Pay"
+for i in Song:
+    sys.stdout.write(i)
+    sys.stdout.flush()
+    time.sleep(0.2)
