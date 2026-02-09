13aa5c4930a60574244dea5f19bb953075415f09
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-02-09 19:59:00 +0500

    .

--- song.py
+++ song.py
@@ -1,19 +1,27 @@
-import sys, time
-Song = "Dil meri na suny\n Dil ki main na sunu"
-for i in Song:
-    sys.stdout.write(i)
-    sys.stdout.flush()
-    time.sleep(0.1)
+# import sys, time
+# Song = "Dil meri na suny\n Dil ki main na sunu"
+# for i in Song:
+#     sys.stdout.write(i)
+#     sys.stdout.flush()
+#     time.sleep(0.1)
 
+# import sys, time
+# Song = "\nMeri Zindghi Hai Tu\n Tu Jo Chahy Bol Na Pay"
+# for i in Song:
+#     sys.stdout.write(i)
+#     sys.stdout.flush()
+#     time.sleep(0.2)
+# import sys, time
+# Song = "\nAy Dil Hai Mushkil\n Jb Jb Tum Ko Dekhun "
+# for i in Song:
+#     sys.stdout.write(i)
+#     sys.stdout.flush()
+#     time.sleep(0.2)
+# calculator = input("Enter a  number:")
+# print(eval(calculator))
 import sys, time
-Song = "\nMeri Zindghi Hai Tu\n Tu Jo Chahy Bol Na Pay"
-for i in Song:
-    sys.stdout.write(i)
-    sys.stdout.flush()
-    time.sleep(0.2)
-import sys, time
-Song = "\nAy Dil Hai Mushkil\n Jb Jb Tum Ko Dekhun "
-for i in Song:
+Naat = "\nTajdar-e-Haram\n Ho Nigah-e-Karam"
+for i in Naat:
     sys.stdout.write(i)
     sys.stdout.flush()
     time.sleep(0.2)
