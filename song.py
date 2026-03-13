05c4b68946ffe77c3f8abfe9821591418764724b
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-03-13 12:43:28 +0500

    .

--- song.py
+++ song.py
@@ -86,6 +86,9 @@
 # for i in range(0, n+1):
 #     print(" * " * i)
 
-import sqlite3 as sql
-conn = sql.connect("Mydatabase.db")
-print("Database created successfully")
+# import sqlite3 as sql
+# conn = sql.connect("Mydatabase.db")
+# print("Database created successfully")
+stars= 3
+for i in range(0,stars+1):
+    print(" *" *i)
