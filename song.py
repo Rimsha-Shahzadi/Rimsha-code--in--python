d9227361d3ae2a1275833680d3e496a7d70adf6a
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-03-13 12:26:49 +0500

    .

--- song.py
+++ song.py
@@ -82,6 +82,10 @@
 # 
 # print("Rimsha,Jannat")  
 
-n = 5 
-for i in range(0, n+1):
-    print(" * " * i)
+# n = 5 
+# for i in range(0, n+1):
+#     print(" * " * i)
+
+import sqlite3 as sql
+conn = sql.connect("Mydatabase.db")
+print("Database created successfully")
