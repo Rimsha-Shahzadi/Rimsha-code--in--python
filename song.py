af823578dcc44a5e6a91f24d91ac008c89ad4bb2
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-04-17 07:27:49 +0500

    .

--- song.py
+++ song.py
@@ -144,13 +144,13 @@
 #     print(" *" *i)
 
 
-stars = 10
-for i in range(0,stars+1):
-    print(" *" *i)
+# stars = 10
+# for i in range(0,stars+1):
+#     print(" *" *i)
 
-stars = 8
-for i in range(0,stars+1):
-    print(" *" *i)
+# stars = 8
+# for i in range(0,stars+1):
+#     print(" *" *i)
 table = int(input("Enter a number:")) 
 for i in range(1,11): 
     print(f"{table} x {i} = {table*i}")  
