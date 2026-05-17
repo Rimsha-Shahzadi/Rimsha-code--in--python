ec54e84e4400b96c5194ddd59d400826c3383db7
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-05-17 09:26:37 +0500

    .

--- sorry.py
+++ sorry.py
@@ -18,7 +18,12 @@
 #     print("Sorry")
 #     i+=1    
 
-i=5
-while i<= 1:
+# i=5
+# while i<= 1:
+#     print("Sorry")
+#     i+=1  
+
+i=1
+while i<= 5:
     print("Sorry")
-    i+=1  
+    i+=1 
