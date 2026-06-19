a8b678a5e1476389dafa51f4254db185985fb500
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-06-19 07:37:20 +0500

    .

---
+++ replit.py
@@ -0,0 +1,299 @@
+"""Hey! Rimsha don't remove this code
+Author = Rimsha
+Course = Python"""
+'''print("Hey I am a /"good girl/"/nand, this viewer is also a good girl")'''
+print("Hello World")
+print("Hello World, Rim, Jim")
+print(23*89)
+print("Rimsha")
+print("Jannat")
+print("Allah Hafiz")
+# print("Hello"[1:4])
+# dict = {'a': 1, 'b': 2}; print(dict['a'])
+# print({1, 2, 2, 3} - {2, 3})
+# def add(a, b=5):
+#     return a + b
+# print(add(3))
+# x = 10
+# if x > 5:
+#     print("Greater")
+# else:
+#     print("Smaller")
+# print(5//2)
+# print(2 + 3 * 4)
+# print(10 % 3)
+# print(not(True and False))
+# x = 7
+# if x < 10:
+#     print("Less")
+# elif x == 10:
+#     print("Equal")
+# else:
+#     print("More")
+# x = 0
+# while x < 3:
+#   print(x)
+#   x += 1
+# def greet(name="Guest"):
+#     return f"Hello, {name}"
+# print(greet("Alice"))
+# tuple1 = (1,); print(type(tuple1))
+# list2 = [1, 2, 3]; list2[1] = 5; print(list2)
+# print({1, 2, 3} | {3, 4, 5})
+# dict1 = {'a': 1, 'b': 2}; print(dict1.get('c', 0))
+# dict2 = {'x': 10, 'y': 20}; print(len(dict2))
+# print("Python".upper())
+# print("hello " + "world")
+# import math; print(math.pi)
+# class Test:
+#     x = 10
+# print(Test.x)
+# try:
+#     print(10 / 0)
+# except ZeroDivisionError:
+#     print("Error")
+# def deco(func):
+#     def inner():
+#         return func() + 1
+#     return inner
+
+# @deco
+# def num():
+#     return 5
+# print(num())
+# import re; print(bool(re.search('a', 'cat')))
+# import re; print(re.sub('a', 'b', 'apple'))
+# import re; print(re.match('a', 'cat'))
+# def deco(func):
+#     def wrapper():
+#         return func() * 2
+#     return wrapper
+
+# @deco
+# def num():
+#     return 3
+# print(num())
+# print("Hey", 6, 7, sep="~", end="009\n")
+# print("Rimsha")
+# a = 2
+# print(a)
+# a1 = 10
+# print(a1)
+# b = "Rimsha"
+# print(b)
+# c = True
+# print(c)
+# d = None
+# print(d)
+# print("the type of a is", type(a))
+# print("the type of b is", type(b))
+# print("the type of c is", type(c))
+# print("the type of d is", type(d))
+# list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+# print(list1)
+
+# tuple1 = (("parrot", "sparrow"), ("lion", "tiger"))
+# print(tuple1)
+
+# dict1 = {"name": "Rimsha", "age": 20, "canVote": True}
+# print(dict1)
+
+# def add(a=39,b=40):
+#     return a+b
+# try:
+#     x = 1 / 0
+# except:
+#     print("Exception")
+#     class Person:
+#         def __init__(self, name):
+#             self.name = name
+#         def say(self):
+#             return self.name
+#     p = Person("Alice")
+#     print(p.say())
+# a = 60
+# b = 30
+# print("The value of ", a, "+", 30,"is:", a+b)
+# print("The value of ", a, "-", 30,"is:", a-b)
+# print("The value of ", a, "*", 30,"is:", a*b)
+# print("The value of ", a, "/", 30,"is:", a/b)
+# a = "1"
+# b = "2"
+# print(a+b)
+# print(int(a)+int(b))
+# string= "15"
+# number = 7
+# string_number = int(string)
+# sum = number + string_number
+# print("The sum of both numbers is:", sum)
+# a = 9
+# print(type(a))
+# b =3.9
+# print(type(b))
+# c =a+b
+# print(c)
+# print(type(c))
+# a = input("Enter your name :")
+# print("My name is", a)
+# x = input("Enter your first number:")
+# y = input("Enter your second number:")
+# print (x + y)
+# print(int(x) + int(y))
+
+# name = "Rimsha"
+# friend ="Tahi"
+# anotherFriend = "Jannat"
+# apple = '''He said,
+# Hi Rimsha
+# I am good
+# I want to eat an apple'''
+
+# print("Hello", friend, name, anotherFriend, apple)
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# print("Lets use a for loop\n")
+# for character in  apple:
+#     print(character)
+
+# names = "Rimsha, Jannat, Tahi"
+# print(names[0:6])
+# print(len(names))
+# fruit = "Mango"
+# mangoLen= len(fruit)
+# print(mangoLen)
+# print(fruit[0:-3])
+# print(fruit[0:len(fruit)-3])
+# nm = "Harry"
+# print(nm[-4:-2])
+# strings are immutable
+# a = "Rimsha !!!!!!!!! Rimsha"
+# print(len(a))
+# print(a)
+# print(a.upper())
+# print(a.lower())
+# print(a.rstrip("!"))
+# print(a.replace("Rimsha", "Jannat"))
+# print(a.split(" "))
+# blogheading = "introduction to js"
+# print(blogheading.capitalize())
+# str1 = "Welcome to the console!!!"
+# print(len(str1))
+# print(str1.center(30))
+# print(a.count("Rimsha"))
+# print(str1.endswith("!!!!!"))
+# str1 = "Welcome to the console!!!"
+# print(str1.endswith("to", 4, 10))
+# str1 = "She's Rimsha.She is very good girl."
+# print(str1.find('is'))
+# print(str1.index("ish"))
+# str1 = "WelcomeToTheConsole"
+# print(str1.isalnum())
+# str1 = "Welcome"
+# print(str1.isalpha())
+# str1 ="rimsha is a good girl"
+# print(str1.islower())
+# str1 ="We wish you a Merry Christmas"
+# print(str1.isprintable())
+# str1= "          "
+# print(str1.isspace())
+# str1 ="World Health Organization"
+# print(str1.istitle())
+# str2 = "Python is interpreted language"
+# print(str2.swapcase())
+# str2 ="He's name is dan. He is an honest man."
+# print(str2.title())
+
+# if else
+# a = int(input("Enter your age: "))
+# print("your age is:", a)
+# print(a>18)
+# print(a<18)
+# print(a>=18)
+# print(a<=18)
+# print(a!=18)
+
+# if(a>18):
+#     print("you can drive")
+# else:
+#   print("you cannot drive")
+
+# exercise 1
+# a = 20
+# b = 50
+# print("the value of ", a,"+",b,"is",a+b)
+# print("the value of ", a,"-",b,"is",a-b)
+# print("the value of ", a,"*",b,"is",a*b)
+# print("the value of ", a,"/",b,"is",a/b)
+
+# name = "Rimsha"
+# for i in name:
+#   print(i, end=",")
+#   if(i=="b"):
+#     print("Something is special")
+
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for color in colors:
+#   print(color)
+#   for i in color:
+#     print(i)
+# for k in range(500):
+#   print(k)
+# for k in range(1,30,3):
+#   print(k+2)
+# i = 10
+# while (i <= 40):
+#   i = int(input("Enter the number:"))
+#   print(i)
+
+# print("Done with loop")
+# for i in range(20):
+#   if (i == 10):
+#     print("skip the iteration")
+#     continue
+#   print("5 x", i + 1, "=", 5 * (i + 1))
+
+# def claculateGmean(a, b):
+#   mean = (a * b) / (a + b)
+#   print(mean)
+
+# def isGreater(a, b):
+#   if (a > b):
+#     print("First number is greater")
+#   else:
+#     print("Second number is greater or equal")
+# def isLesser(a,b):
+#   pass
+# a = 10
+# b = 20
+
+# def average(a=1, b=2):
+#   print("The average is", (a + b) / 2)
+# average(b=2)
+
+
+# def average(*numbers):
+#   print(type(numbers))
+#   sum = 0
+#   for i in numbers:
+#     sum = sum + i
+#     print("Average is:", sum / len(numbers))
+
+
+# average(5, 6)
+
+# marks = [95, 98, 97, "Rimsha", True]
+# print(marks[-3])
+# print(marks[len(marks)-3])
+
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(marks[2])
+# print(marks[3])
+# print(marks[4])
+# print(marks[5])
+# print(marks[6])
+
