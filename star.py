513bc3598a66b897cd37b07c535591feb23fe0b3
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2026-09-15 19:09:47 +0500

    .

--- star.py
+++ star.py
@@ -1,8 +1,8 @@
 # Question1 
-# for i in range(1,6):
-#     for j in range(1,6):
-#         print("*", end=" ")
-#     print() 
+for i in range(1,6):
+    for j in range(1,6):
+        print("*", end=" ")
+    print() 
 #  
 # Question2
 
